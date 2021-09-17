e1d166a1f73aa41342b6b53e1619ac941b81bba4
suryakantas <66069016+suryakantas@users.noreply.github.com>
2021-09-17 14:24:27 +0530

    Add files via upload

---
+++ src/component/success/Success.jsx
@@ -0,0 +1,24 @@
+import "./success.scss"
+
+export default function Success() {
+    return (
+        <div className="success" id="success">
+            <div className="container">
+                <div className="left">
+                    <div className="top">
+                        <h3>What difference us</h3>
+                    </div>
+                    <div className="center">
+                        <li>Odisha's best Ambulance Service</li>
+                        <li>Odisha's only</li>
+                        <li>We give primery treatment's</li>
+                        <li>Farmasist for taking care.</li>
+                    </div>
+                </div>
+                <div className="right">
+                    <img src="assets/ambulance.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
