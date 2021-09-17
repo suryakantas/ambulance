4cf719df9e69de77ce2745b1c8dbaa2a8e014b47
suryakantas <66069016+suryakantas@users.noreply.github.com>
2021-09-17 14:26:13 +0530

    Add files via upload

---
+++ src/component/advertise/Advertise.jsx
@@ -0,0 +1,25 @@
+import "./advertise.scss"
+
+export default function Advertise() {
+    return (
+        <div className="advertise" id="advertise">
+            <div className="container">
+                <div className="left">
+                <img src="assets/ambulance.jpg" alt="" />
+                </div>
+                <div className="right">
+                   
+                    <div className="top">
+                        <h3>About Our Facilities</h3>
+                    </div>
+                    <div className="center">
+                        <li>Odisha's best Ambulance Service</li>
+                        <li>Odisha's only</li>
+                        <li>We give primery treatment's</li>
+                        <li>Farmasist for taking care.</li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
