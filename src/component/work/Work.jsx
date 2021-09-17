f118c521cf3636b92159398a6f4113e89579c5d0
suryakantas <66069016+suryakantas@users.noreply.github.com>
2021-09-17 08:45:23 +0530

    Add files via upload

---
+++ src/component/work/Work.jsx
@@ -0,0 +1,38 @@
+import "./work.scss";
+
+export default function Work() {
+    return (
+        <div className="work" id="work">
+           <h1>Why Choose Us?</h1>
+           <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/mobile.jpg" className="left" alt="" />
+                        </div>
+                        <div className="center">
+                            <h3>Sritam Salto</h3>
+                        </div>
+                    <div className="bottom"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempore quia incidunt sapiente voluptate inventore magnam minima commodi aspernatur iste error consectetur veniam amet!</div>
+                </div>
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/mobile.jpg" className="left" alt="" />
+                        </div>
+                        <div className="center">
+                            <h3>Sritam Salto</h3>
+                        </div>
+                    <div className="bottom"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempore quia incidunt sapiente voluptate inventore magnam minima commodi aspernatur iste error consectetur veniam amet!</div>
+                </div>
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/mobile.jpg" className="left" alt="" />
+                        </div>
+                        <div className="center">
+                            <h3>Sritam Salto</h3>
+                        </div>
+                    <div className="bottom"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempore quia incidunt sapiente voluptate inventore magnam minima commodi aspernatur iste error consectetur veniam amet!</div>
+                </div>
+           </div>
+        </div>
+    )
+}
