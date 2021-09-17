6377883128f3ff1d8b87795f62adeec068a49b26
suryakantas <66069016+suryakantas@users.noreply.github.com>
2021-09-17 08:46:19 +0530

    Add files via upload

---
+++ src/component/contact/Contact.jsx
@@ -0,0 +1,17 @@
+import "./contact.scss"
+
+export default function Contact() {
+    return (
+        <div className="contact" id="contact">
+            {/* <div className="container">
+                <div className="top">
+                    <h3>Want to grow your business?</h3>
+                    <h3>We Should talk</h3>
+                </div>
+                <div className="center">
+                    <button></button>
+                </div>
+            </div> */}
+        </div>
+    )
+}
